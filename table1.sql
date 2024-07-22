b0f19969d252886bd9080c52d62de697615d17f1
harshdipsinhJ <168002710+harshdipsinhJ@users.noreply.github.com>
2024-07-22 21:29:56 +0530

    table1.sql

---
+++ table1.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE college;
+USE college;
+CREATE TABLE harsh(
+id INT,
+name VARCHAR(50),
+age int
+);
+INSERT INTO harsh(id,name,age) VALUES(11,"HARSHDIP",24);
+INSERT INTO harsh(id,name,age) VALUES(14,"DIP",27);
+INSERT INTO harsh(id,name,age) VALUES(1,"HARSH",2);
+
+SELECT * FROM harsh;
